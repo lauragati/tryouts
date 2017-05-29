c11fadc559eab81e8f664b118221f96a60c95ad8
lauragati <lauraember@gmail.com>
2017-05-29 12:30:22 -0400

    Update another_matlab.m

--- another_matlab.m
+++ another_matlab.m
@@ -4,3 +4,7 @@
 % what if I edit this guy on Matlab: can I now sync it to the ether?
 % ->> yess! holly smolly, it works! I edit it here and click `view and
 % commit changes' or whatever WITHIN matlab!!!!
+
+% Then it appears in GitHub Desktop. There you sync to put it on the ether.
+
+% Now I'm editing it in the web: let's see if it appears in Matlab. 
